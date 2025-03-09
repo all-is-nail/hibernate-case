f5eb1791ce8c0505b27338233e848aa8ac37e2ed
m0v1 <zheng9feng@vip.qq.com>
2025-03-09 22:29:14 +0800

    test: Add StatelessSessionTest for bulk insert and session management

---
+++ stateless-session-case/src/test/java/org/example/StatelessSessionTest.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.example.config.AppConfig;
+import org.example.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class StatelessSessionTest {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Test
+    public void testStatelessSession() {
+        StatelessSession statelessSession = sessionFactory.openStatelessSession();
+        statelessSession.beginTransaction();
+
+        User user = new User();
+        user.setName("test");
+        user.setEmail("aa@a.com");
+        statelessSession.insert(user);
+        statelessSession.getTransaction().commit();
+
+        statelessSession.close();
+    }
+
+    @Test
+    public void testStatelessSessionWithBulkInsert() {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(
+                new HibernateTransactionManager(sessionFactory));
+
+        transactionTemplate.execute(status -> {
+            StatelessSession statelessSession = sessionFactory.openStatelessSession();
+
+            try {
+                for (int i = 0; i < 10; i++) {
+                    User user = new User();
+                    user.setName("test" + i);
+                    user.setEmail("aa@a.com");
+                    statelessSession.insert(user);
+                }
+
+                return null;
+            } finally {
+                statelessSession.close();
+            }
+        });
+
+        try (Session session = sessionFactory.openSession()) {
+            List<User> users = session.createQuery("from User where name like 'test%'", User.class).list();
+            assertEquals("Should have inserted 10 users", 10, users.size());
+            for (int i = 0; i < 10; i++) {
+                User user = users.get(i);
+                assertEquals("test" + i, user.getName());
+                assertEquals("aa@a.com", user.getEmail());
+            }
+        }
+    }
+    
+}
